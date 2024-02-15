76574d00ef35c711b58c9824cc1c9b4e15b07c38
ezomo <kiruruman@gmail.com>
2024-02-15 19:11:51 +0900

    push

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ use safe_drive::{
     topic::{publisher::{self, Publisher}, subscriber::Subscriber},
     RecvResult,
 };
-
-
+use crate::std_msgs::msg;
 use std::{rc::Rc, time::Duration};
 use drobo_interfaces::msg::MdLibMsg;
 
@@ -25,6 +24,8 @@ enum CANNON {
 
 const RECOVERY:u8 = 6;
 const ISOLATION:u8 = 7;
+const _CHARGE:u8 = 8;
+
 
 pub mod DualsenseState {
     pub const SQUARE: usize = 0;
@@ -50,14 +51,17 @@ fn main() -> Result<(), DynError> {
 
     let selector = ctx.create_selector()?;
     let subscriber = node.create_subscriber::<sensor_msgs::msg::Joy>("joy", None)?;
-    let publisher = node.create_publisher::<drobo_interfaces::msg::MdLibMsg>("md_driver_topic", None)?;
+    let publisher_msd = node.create_publisher::<msg::Bool>("servo_atonement_topic", None)?;
+    let publisher_md = node.create_publisher::<drobo_interfaces::msg::MdLibMsg>("md_driver_topic", None)?;
 
 
 
     worker(
         selector,
         subscriber,
-        publisher
+        publisher_md,
+        publisher_msd
+        
     )?;
     Ok(())
 }
@@ -65,39 +69,56 @@ fn main() -> Result<(), DynError> {
 fn worker(
     mut selector: Selector,
     subscriber: Subscriber<sensor_msgs::msg::Joy>,
-    publisher:Publisher<MdLibMsg>
+    publisher_md:Publisher<MdLibMsg>,
+    publisher_msd:Publisher<msg::Bool>
 ) -> Result<(), DynError> {
     let mut p9n = p9n_interface::PlaystationInterface::new(sensor_msgs::msg::Joy::new().unwrap());
-    // let logger = Rc::new(Logger::new("controller_2023"));
+    let _logger = Rc::new(Logger::new("controller_2023"));
 
+    let mut charge_state = true;
     selector.add_subscriber(
         subscriber,
         Box::new(move |_msg| {
             p9n.set_joy_msg(_msg.get_owned().unwrap());
 
             if p9n.pressed_r2() {
-                cannon_controller(500,true,&publisher);
+                cannon_controller(500,true,&publisher_md);
             }
             if !p9n.pressed_r2() {
-                cannon_controller(0,false,&publisher);
+                cannon_controller(0,false,&publisher_md);
             }
 
 
             if p9n.pressed_r1() {
-                recovery_controller(999,true,&publisher);
+
+                recovery_controller(400,true,&publisher_md);
             }
             if !p9n.pressed_r1() {
-                recovery_controller(0,false,&publisher);
+                recovery_controller(0,false,&publisher_md);
             }
 
 
 
             if p9n.pressed_dpad_down(){
-                isolation_controller(500,true,&publisher);
+                isolation_controller(500,true,&publisher_md);
             }
             if !p9n.pressed_dpad_down(){
-                isolation_controller(0,true,&publisher);
+                isolation_controller(0,true,&publisher_md);
             }
+
+
+
+
+            if p9n.pressed_dpad_left()  && charge_state{
+                charge_contorller(&publisher_msd);
+                charge_state = false;
+
+            }
+            if !p9n.pressed_dpad_left(){
+                charge_state = true;
+
+            }
+
 
 
 
@@ -109,47 +130,55 @@ fn worker(
 }
 
 
-fn cannon_controller(power:u16,mode:bool,publisher:&Publisher<MdLibMsg>){
+fn cannon_controller(power:u16,mode:bool,publisher_md:&Publisher<MdLibMsg>){
 
     if mode {
-        send_pwm(CANNON::R as u8,0,false,power,&publisher);
-        send_pwm(CANNON::L as u8,0,true,power,&publisher);
+        send_pwm(CANNON::R as u8,0,true,power,&publisher_md);
+        send_pwm(CANNON::L as u8,0,false,power,&publisher_md);
 
         return;
 
     }
 
-    send_pwm(CANNON::R as u8,0,true,0,&publisher);
-    send_pwm(CANNON::L as u8,0,true,0,&publisher);
+    send_pwm(CANNON ::R as u8,0, true,0,&publisher_md);
+    send_pwm(CANNON::L as u8,0,false,0,&publisher_md);
 }
 
 
-fn recovery_controller(power:u16,mode:bool,publisher:&Publisher<MdLibMsg>){
+fn recovery_controller(power:u16,mode:bool,publisher_md:&Publisher<MdLibMsg>){
 
     if mode {
-        send_pwm(RECOVERY,0,true,power,&publisher);
+        send_pwm(RECOVERY,0,true,power,&publisher_md);
         return;
 
     }
 
-    send_pwm(RECOVERY,0,true,0,&publisher);
+    send_pwm(RECOVERY,0,true,0,&publisher_md);
 
 }
 
-fn isolation_controller(power:u16,mode:bool,publisher:&Publisher<MdLibMsg>){
+
+fn isolation_controller(power:u16,mode:bool,publisher_md:&Publisher<MdLibMsg>){
 
     if mode {
-        send_pwm(ISOLATION,0,true,power,&publisher);
+        send_pwm(ISOLATION,0,true,power,&publisher_md);
         return;
 
     }
 
-    send_pwm(ISOLATION,0,true,0,&publisher);
+    send_pwm(ISOLATION,0,true,0,&publisher_md);
 
 }
 
 
-fn send_pwm(_address:u8, _semi_id:u8,_phase:bool,_power:u16,publisher:&Publisher<MdLibMsg>){
+fn charge_contorller(publisher_msd:&Publisher<msg::Bool>){
+    let mut msg = crate::std_msgs::msg::Bool::new().unwrap();
+    msg.data = true;
+    publisher_msd.send(&msg).unwrap()
+}
+
+
+fn send_pwm(_address:u8, _semi_id:u8,_phase:bool,_power:u16,publisher_md:&Publisher<MdLibMsg>){
     let mut msg = drobo_interfaces::msg::MdLibMsg::new().unwrap();
     msg.address = _address as u8;
     msg.semi_id = _semi_id as u8;
@@ -157,6 +186,6 @@ fn send_pwm(_address:u8, _semi_id:u8,_phase:bool,_power:u16,publisher:&Publisher
     msg.phase = _phase as bool;
     msg.power = _power as u16;
 
-    publisher.send(&msg).unwrap()
+    publisher_md.send(&msg).unwrap()
 
 }
